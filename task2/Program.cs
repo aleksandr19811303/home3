66f96ae32f13c0c025656a550a35798ee9c6964b
Aleksandr <shbragnikov@inbox.ru>
2023-02-27 18:01:55 +0300

    2

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу, которая принимает на вход число и выдает сумму цифр в числе.
+int Prompt(string message)
+{
+    System.Console.Write(message); // Выводим приглашение ко вводу
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput); // приводим к числу
+    return result; // возвращаем результат
+}
+
+int SumAllDigit(int number)
+{
+   int result = 0;
+   while (number > 0)
+  {
+    result += number % 10;
+    number = number / 10;
+  }
+  return result;
+}
+
+int number = Prompt("Введите число: ");
+System.Console.WriteLine($"Сумма всех чисел в цифре {number} = {SumAllDigit (number)}");
+
